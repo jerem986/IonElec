7a4c44066ec11fce1b57ef68be2c3de9f0914705
Argyropoulos Jérémy <jeremy.argyropoulos-partner@aperam.com>
2025-04-21 22:27:08 +0200

    first step on data service

---
+++ src/app/core/data-service.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { CreateMonthlyReportCommand } from '@shared/commands';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataServiceService {
+
+  constructor() { }
+
+  public async createMonthlyReport(command: CreateMonthlyReportCommand): Promise<void> {
+console.log('todo')
+// implemets .vscode for format when ctrl + s
+  }
+}
