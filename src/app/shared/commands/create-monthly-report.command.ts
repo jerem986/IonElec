594b02abd82158965dd5fb7b24953f10bf839395
Argyropoulos Jérémy <jeremy.argyropoulos-partner@aperam.com>
2025-04-22 21:57:33 +0200

    adjust CreateMonthlyReportCommand

--- src/app/shared/commands/create-monthly-report.command.ts
+++ src/app/shared/commands/create-monthly-report.command.ts
@@ -1,9 +1,10 @@
-
 import { CommandBase } from './command.base';
 
 export class CreateMonthlyReportCommand extends CommandBase {
-    public dayCounter: number| null = null;
-    public nightCounter: number| null = null;
-    public productionCounter: number| null = null;
-    public carCounter: number| null = null;
+	public dayCounter: number | null = null;
+	public nightCounter: number | null = null;
+	public productionCounter: number | null = null;
+	public carCounter: number | null = null;
+	public month: number | null = null;
+	public year: number | null = null;
 }
