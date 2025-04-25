b138b76e0ad557987bf8d08ded770f77a088940f
Argyropoulos Jérémy <jeremy.argyropoulos-partner@aperam.com>
2025-04-25 22:23:12 +0200

    WIP tab2 add input

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { MyInputComponent } from '@shared/components/my-input/my-input.component';
+import { CreateMonthlyReportCommand } from '@shared/commands';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+	selector: 'app-tab2',
+	templateUrl: 'tab2.page.html',
+	styleUrls: ['tab2.page.scss'],
+	imports: [IonHeader, IonToolbar, IonTitle, IonContent, MyInputComponent, FormsModule],
 })
 export class Tab2Page {
+	public command = new CreateMonthlyReportCommand();
 
-  constructor() {}
-
+	constructor() {}
 }
