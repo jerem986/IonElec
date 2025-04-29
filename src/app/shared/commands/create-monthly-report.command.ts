59777b3b02efb7fdec7a91caa90b41c56c9e3505
Argyropoulos Jérémy <jeremy.argyropoulos-partner@aperam.com>
2025-04-29 22:30:11 +0200

    adapt command with new validators

--- src/app/shared/commands/create-monthly-report.command.ts
+++ src/app/shared/commands/create-monthly-report.command.ts
@@ -1,8 +1,33 @@
+import { Min, IsNotEmpty, IsNumber, Max } from 'class-validator';
+
 export class CreateMonthlyReportCommand {
+	@IsNotEmpty()
+	@IsNumber()
+	@Min(0)
 	public dayCounter!: number;
+
+	@IsNotEmpty()
+	@IsNumber()
+	@Min(0)
 	public nightCounter!: number;
+
+	@IsNotEmpty()
+	@IsNumber()
+	@Min(0)
 	public productionCounter!: number;
+
+	@IsNotEmpty()
+	@IsNumber()
+	@Min(0)
 	public carCounter!: number;
+
+	@IsNotEmpty()
+	@IsNumber()
+	@Min(0)
+	@Max(11)
 	public month!: number;
+
+	@IsNotEmpty()
+	@IsNumber()
 	public year!: number;
 }
