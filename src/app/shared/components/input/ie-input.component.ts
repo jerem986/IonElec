8d1ee961216916a804dab75167ec9fde590b14e8
Jeremy Argyropoulos <jeremy.argyropoulos-partner@aperam.com>
2025-04-30 08:16:47 +0200

    adapt input

--- src/app/shared/components/input/ie-input.component.ts
+++ src/app/shared/components/input/ie-input.component.ts
@@ -1,32 +1,19 @@
-import { Component, input, signal, OnInit, Signal, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
+import { FormElement } from '../form-element';
+import { input } from '@angular/core';
 
 @Component({
 	selector: 'ie-input',
-	templateUrl: './ie-input.component.html',
+	standalone: true,
 	imports: [CommonModule, MatFormFieldModule, MatInputModule, FormsModule],
+	templateUrl: './ie-input.component.html',
 })
-export class InputComponent {
+export class InputComponent extends FormElement<string | number> {
 	public label = input<string>('');
 	public placeholder = input<string>('');
-	public type = input<'text' | 'number'>();
-
-	@Input() public valueSignal?: Signal<string | number>;
-
-	private internalValue = signal<string | number>('');
-
-	public get value(): Signal<string | number> {
-		return this.valueSignal ?? this.internalValue;
-	}
-
-	public setValue(newValue: string | number): void {
-		if (this.valueSignal) {
-			(this.valueSignal as any).set(newValue);
-		} else {
-			this.internalValue.set(newValue);
-		}
-	}
+	public type = input<'text' | 'number'>('text');
 }
