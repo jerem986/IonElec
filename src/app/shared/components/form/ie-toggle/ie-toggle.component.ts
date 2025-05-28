dba3dc45e8d61a2d17a1e3b3b01cadaf693f3364
Argyropoulos Jérémy <jeremy.argyropoulos-partner@aperam.com>
2025-05-28 22:50:39 +0200

    add IeToggleComponent

---
+++ src/app/shared/components/form/ie-toggle/ie-toggle.component.ts
@@ -0,0 +1,18 @@
+import { Component, input, output } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+	selector: 'ie-toggle',
+	imports: [IonicModule],
+	templateUrl: './ie-toggle.component.html',
+})
+export class IeToggleComponent {
+	public label = input<string>('');
+	public checked = input<boolean>(false);
+	public disabled = input<boolean>(false);
+	public checkedChange = output<boolean>();
+
+	onToggle(event: any) {
+		this.checkedChange.emit(event.detail.checked);
+	}
+}
