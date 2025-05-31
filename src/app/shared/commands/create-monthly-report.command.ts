b175408c3b2b0c81bdab5b09dfc622e0fa189cd9
Argyropoulos Jérémy <jeremy.argyropoulos-partner@aperam.com>
2025-05-31 22:33:49 +0200

    add validator decorator to command

--- src/app/shared/commands/create-monthly-report.command.ts
+++ src/app/shared/commands/create-monthly-report.command.ts
@@ -1,8 +1,35 @@
+import { PropertyType } from '@shared/decorator/property-type.decorator';
+import { IsNumber, Max, Min } from 'class-validator';
+
 export class CreateMonthlyReportCommand {
+	@IsNumber()
+	@Min(0)
+	@PropertyType(Number)
 	public dayCounter: number = null;
+
+	@IsNumber()
+	@Min(0)
+	@PropertyType(Number)
 	public nightCounter: number = null;
+
+	@IsNumber()
+	@Min(0)
+	@PropertyType(Number)
 	public productionCounter: number = null;
+
+	@IsNumber()
+	@Min(0)
+	@PropertyType(Number)
 	public carCounter: number = null;
+
+	@IsNumber()
+	@Min(0)
+	@Max(11)
+	@PropertyType(Number)
 	public month: number = null;
+
+	@IsNumber()
+	@Min(2020)
+	@PropertyType(Number)
 	public year: number = null;
 }
